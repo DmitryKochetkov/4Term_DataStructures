eef37a3c0becee75386bae54536c396706547ea6
DmitryKochetkov <34890452+DmitryKochetkov@users.noreply.github.com>
2019-02-21 18:04:35 +0300

    Lab1: project created

---
+++ Lab1/StackForm.cpp
@@ -0,0 +1,12 @@
+#include "StackForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Lab1::StackForm form;
+	Application::Run(%form);
+}
---
+++ Lab1/StackForm.h
@@ -0,0 +1,71 @@
+#pragma once
+
+namespace Lab1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for StackForm
+	/// </summary>
+	public ref class StackForm : public System::Windows::Forms::Form
+	{
+	public:
+		StackForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~StackForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// StackForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(10, 20);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(517, 457);
+			this->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->MaximizeBox = false;
+			this->Name = L"StackForm";
+			this->ShowIcon = false;
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"Lab 1";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
