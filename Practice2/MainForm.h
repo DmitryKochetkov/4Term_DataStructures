fd133555c34332952a6f07ee4f5225a1b90bba25
DmitryKochetkov <34890452+DmitryKochetkov@users.noreply.github.com>
2019-03-11 11:36:19 +0300

    Practice 2: Interface

---
+++ Practice2/MainForm.h
@@ -0,0 +1,140 @@
+#pragma once
+
+namespace Practice2 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MainForm
+	/// </summary>
+	public ref class MainForm : public System::Windows::Forms::Form
+	{
+	public:
+		MainForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MainForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::RichTextBox^  richTextBox1;
+	protected:
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::CheckBox^  checkBox1;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->SuspendLayout();
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Location = System::Drawing::Point(33, 49);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(502, 136);
+			this->richTextBox1->TabIndex = 0;
+			this->richTextBox1->Text = L"";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(108, 215);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(427, 22);
+			this->textBox1->TabIndex = 1;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(30, 19);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(49, 17);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"String:";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(30, 215);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(72, 17);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Substring:";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(204, 302);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(156, 38);
+			this->button1->TabIndex = 4;
+			this->button1->Text = L"Search";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			this->checkBox1->Location = System::Drawing::Point(207, 264);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(150, 21);
+			this->checkBox1->TabIndex = 5;
+			this->checkBox1->Text = L"Register Sensitivity";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(564, 370);
+			this->Controls->Add(this->checkBox1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->richTextBox1);
+			this->MaximizeBox = false;
+			this->Name = L"MainForm";
+			this->ShowIcon = false;
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"Practice 2";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	};
+}
