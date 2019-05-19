e608df6255d30812768233f4a04b79422fc6cecf
DmitryKochetkov <34890452+DmitryKochetkov@users.noreply.github.com>
2019-05-20 02:08:39 +0300

    Lab 2: init

--- Lab2/MainForm.h
+++ Lab2/MainForm.h
@@ -10,7 +10,7 @@ namespace Lab2 {
 	using namespace System::Drawing;
 
 	/// <summary>
-	/// Summary for MainForm
+	/// Сводка для MainForm
 	/// </summary>
 	public ref class MainForm : public System::Windows::Forms::Form
 	{
@@ -19,13 +19,13 @@ namespace Lab2 {
 		{
 			InitializeComponent();
 			//
-			//TODO: Add the constructor code here
+			//TODO: добавьте код конструктора
 			//
 		}
 
 	protected:
 		/// <summary>
-		/// Clean up any resources being used.
+		/// Освободить все используемые ресурсы.
 		/// </summary>
 		~MainForm()
 		{
@@ -34,34 +34,104 @@ namespace Lab2 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TreeView^  treeView1;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Button^  reflect;
+
+	protected:
 
 	private:
 		/// <summary>
-		/// Required designer variable.
+		/// Обязательная переменная конструктора.
 		/// </summary>
 		System::ComponentModel::Container ^components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->treeView1 = (gcnew System::Windows::Forms::TreeView());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->reflect = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
+			// 
+			// treeView1
+			// 
+			this->treeView1->Location = System::Drawing::Point(34, 58);
+			this->treeView1->Name = L"treeView1";
+			this->treeView1->Size = System::Drawing::Size(188, 179);
+			this->treeView1->TabIndex = 0;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(31, 28);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(49, 13);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Дерево:";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(262, 71);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 2;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(274, 107);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 3;
+			this->button1->Text = L"Добавить";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MainForm::button1_Click);
+			// 
+			// reflect
+			// 
+			this->reflect->Location = System::Drawing::Point(249, 160);
+			this->reflect->Name = L"reflect";
+			this->reflect->Size = System::Drawing::Size(129, 52);
+			this->reflect->TabIndex = 4;
+			this->reflect->Text = L"Отразить относительно пути максимальной длины";
+			this->reflect->UseVisualStyleBackColor = true;
+			this->reflect->Click += gcnew System::EventHandler(this, &MainForm::reflect_Click);
 			// 
 			// MainForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(426, 309);
-			this->MaximizeBox = false;
+			this->ClientSize = System::Drawing::Size(417, 272);
+			this->Controls->Add(this->reflect);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->treeView1);
 			this->Name = L"MainForm";
 			this->ShowIcon = false;
-			this->Text = L"Lab 2";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"Lab2";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		if (treeView1->SelectedNode == nullptr)
+			treeView1->Nodes->Add(textBox1->Text);
+		else treeView1->SelectedNode->Nodes->Add(textBox1->Text);
+	}
+private: System::Void reflect_Click(System::Object^  sender, System::EventArgs^  e) {
+	TreeView^ treeView2 = gcnew TreeView();
+	
+}
+};
 }
